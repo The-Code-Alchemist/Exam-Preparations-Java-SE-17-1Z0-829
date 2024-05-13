9e4d9fabe068bc2c2670d55a4d9b0c53aaf85755
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-05-13 15:00:53 +0200

    Hello World program with manual compilation/execution instructions

---
+++ introduction/Zoo.java
@@ -0,0 +1,13 @@
+package introduction;
+
+/**
+ *  @author The Code Alchemist
+ *  from Chapter 1: Building Blocks, page 8
+ *  Compile the source file Zoo.java from the root directory of the project             javac introduction/Zoo.java
+ *  Run the newly compiled bytecode Zoo.class from the root directory of the project    java introduction/Zoo
+ */
+public class Zoo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
