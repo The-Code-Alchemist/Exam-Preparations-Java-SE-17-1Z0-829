401ff702ebe134faef179af76ea0e2699ef86789
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-05-21 14:08:03 +0200

    var example

---
+++ var/Var.java
@@ -0,0 +1,16 @@
+package var;
+
+/**
+ *  @author The Code Alchemist
+ *  from Chapter 1: Building Blocks, page 44
+ *  var is not a reserved word
+ */
+
+public class Var {
+    public void var() {
+        var var = "var";
+    }
+    public void Var() {
+        Var var = new Var();
+    }
+}
