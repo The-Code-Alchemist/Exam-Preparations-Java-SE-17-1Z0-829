f7cfc969b1c441acc509303fe8e07affdd62f2ae
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-06-24 15:17:45 +0200

    Chapters 4, use arrays with loops: the real edge case

---
+++ ArraysAndLoops.java
@@ -0,0 +1,19 @@
+/**
+ * @author The Code Alchemist
+ * Chapters 4: Core APIs, page 183 edge case
+ */
+public class ArraysAndLoops {
+    public static void main(String[] args) {
+        var numbers = new int[10];
+        for(int i = 0; i < numbers.length; i++) {
+            // prints the index of this array increased by 5
+            System.out.print(numbers[i] = i + 5);
+        }   System.out.println("");
+
+        var moreNumbers = new int[11];
+        for(int j = 0; j < moreNumbers.length; j++) {
+            // this one only prints 5
+            System.out.print(moreNumbers[j] += 5);
+        }
+    }
+}
