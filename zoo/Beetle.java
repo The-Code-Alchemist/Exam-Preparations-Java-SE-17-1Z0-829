0a430cfc403acea50d1a09f34c0fefcc186fb24c
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-07-19 19:11:37 +0200

    Chapters 6 - constructor with this and super references

---
+++ zoo/Beetle.java
@@ -0,0 +1,26 @@
+/**
+ * @author The Code Alchemist
+ * Chapter 6: Class Design, page 285
+ * this and super example of variables used and their scope
+ */
+package zoo;
+class Insect {
+    protected int numberOfLegs = 4;
+    String label = "buggy";
+}
+
+public class Beetle extends Insect {
+    protected int numberOfLegs = 6;
+    short age = 3;
+    public void printData() {
+        System.out.println(this.label);
+        System.out.println(super.label);
+        System.out.println(this.age);
+//        System.out.println(super.age);  // will not compile, age only in subclass
+        System.out.println(numberOfLegs);
+        System.out.println(super.numberOfLegs);
+    }
+    public static void main(String[] args) {
+        new Beetle().printData();
+    }
+}
