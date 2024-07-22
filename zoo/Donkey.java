dcd0f4723e9ff0f5e6b858e32dda37cb319cfca6
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-07-22 16:41:53 +0200

    Chapter 6 - constructor equivalency breakdown: how they are equivalent

---
+++ zoo/Donkey.java
@@ -0,0 +1,16 @@
+/**
+ * @author The Code Alchemist
+ * Chapter 6: Class Design, page 294, 295
+ * Constructor equivalency - the declared class is equivalent to the commented out code
+ */
+package zoo;
+
+// public class Donkey {}
+
+//public class Donkey { public Donkey() {} }
+
+public class Donkey {
+    public Donkey() {
+        super();
+    }
+}
