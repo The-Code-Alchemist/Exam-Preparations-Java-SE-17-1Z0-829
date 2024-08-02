5dc1b885f96958a4f1d944239a7b71aa40adb00a
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-08-02 10:45:04 +0200

    Chapter 6 - this example added

--- zoo/Crocodile.java
+++ zoo/Crocodile.java
@@ -7,15 +7,19 @@ package zoo;
 public class Crocodile extends Reptile {
     protected int speed = 20;   // without this field, line 15 shows 10
     public int getSpeed() {
-        return speed;           // super.speed calls the speed value from Reptile
+        return speed;
+    }
+    public int getThisSpeed() {
+        return this.speed;      // calling the speed field on the current object
     }
     public int getSuperSpeed() {
-        return super.speed;
+        return super.speed;     // super.speed calls the speed value from Reptile
     }
     public static void main(String[] args) {
         var croc = new Crocodile();
         System.out.println(croc);
         System.out.println(croc.getSpeed());        // 20
+        System.out.println(croc.getThisSpeed());    // 20
         System.out.println(croc.getSuperSpeed());   // 10
     }
 }
