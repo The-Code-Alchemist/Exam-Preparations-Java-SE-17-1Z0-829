2dadc6247f41d98233a6feb333d94498ab648993
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-08-26 11:30:02 +0200

    Chapter 3 - added method: pattern matching for filtering data out, extra javadoc comment added

--- making_decisions/PatternMatching.java
+++ making_decisions/PatternMatching.java
@@ -1,6 +1,6 @@
 /**
  * @author The Code Alchemist
- * Chapter 3: Making Decisions, page 106, 107
+ * Chapter 3: Making Decisions, page 106, 107, 108, 109
  */
 package making_decisions;
 public class PatternMatching {
@@ -8,13 +8,15 @@ public class PatternMatching {
         Integer x = 5;
         compareIntegersWithoutPatternMatching(x);
         compareIntegersWithPatternMatching(x);
-        printIntegerGreaterThan5(x);
+        printIntegerOrNumberGreaterThan5(x);
+        printIntegerTwice(x);
+        printOnlyIntegers(x);
     }
 
     /**
      * @param number
      */
-    public static void compareIntegersWithoutPatternMatching(Number number) {
+    private static void compareIntegersWithoutPatternMatching(Number number) {
         if(number instanceof Integer) {
             Integer data = (Integer) number; // becomes redundant with pattern matching
 
@@ -26,7 +28,7 @@ public class PatternMatching {
     /**
      * @param number
      */
-    public static void compareIntegersWithPatternMatching(Number number) {
+    private static void compareIntegersWithPatternMatching(Number number) {
         /**
          * By making the data parameter final, reassignment can be prevented
          * which is a bad practice with pattern matching
@@ -44,10 +46,36 @@ public class PatternMatching {
 
     /**
      * Pattern matching includes expressions that can be used to filter data out, such as the next example
+     * so that the if statement can be executed only in specific circumstances.
+     *
+     * If the && were to replaced by ||, this method would not compile.
+     * If the input would not inherit Integer, the data variable would be undefined.
+     * The compiler cannot guarantee that data is an instance of java.lang.Integer
      * @param number
      */
-    public static void printIntegerGreaterThan5(Number number) {
+    private static void printIntegerOrNumberGreaterThan5(Number number) {
         if(number instanceof Integer data && data.compareTo(5)>0)
             System.out.println(":" + data);
+    }
+
+    /**
+     * Variable data out of scope after if statement
+     * @param number
+     */
+    private static void printIntegerTwice(Number number) {
+        if(number instanceof Integer data)
+            System.out.println(data.intValue());
+//        System.out.println(data.intValue());  // out of scope
+    }
+
+    /**
+     * This method returns if the input does NOT inherit java.lang.Integer
+     * Therefore, data stays in scope, even after the if statement ends
+     * @param number
+     */
+    private static void printOnlyIntegers(Number number) {
+        if(!(number instanceof Integer data))
+            return;
+        System.out.println(data.intValue());
     }
 }
