dede3cbb5138d7a3d35717fad372e65ce537cf18
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-11-12 13:30:11 +0100

    Chapter 4 - Core APIs String Format flag examples added

--- coreAPIs/StringFormattingValues.java
+++ coreAPIs/StringFormattingValues.java
@@ -5,6 +5,7 @@
  * public static String format(String format, Object args...)
  * public static String format(Locale loc, String format, Object args...)
  * public String formatted(Object args...)
+ * The formatting can be used with optinal flags
  * Mixing data types may cause an exception at runtime
  */
 package coreAPIs;
@@ -27,6 +28,17 @@ public class StringFormattingValues {
 
         // The following causes an IllegalFormatConversionException at runtime even if the variable str is not used
         // A floating-point number is used when an integer is expected
-        str = "Food: %d tons".formatted(2.0);
+        // str = "Food: %d tons".formatted(2.0);     // uncomment this line to reproduce the runtime exception
+
+        // example of String format with flags
+        var pi = 3.14159265359;
+        System.out.format("[%f]", pi);      // [3.141593]       - default
+        System.out.format("[%12.8f]", pi);  // [  3.14159265]   - 12 characters in total, 8 decimal round off whitespace in front of 3
+        System.out.format("[%012f]", pi);   // [00003.141593]   - default with 12 characters in total, 0s in front of 3
+        System.out.format("[%12.2f]", pi);  // [        3.14]   - 12 characters in total, 2 decimal round off whitespace in front of 3
+        System.out.format("[%.3f]", pi);    // [3.142]          - 3 decimal round off
+
+        // The real value of pi in Java
+        System.out.println("The real value of Pi in Java: " + Math.PI);
     }
 }
