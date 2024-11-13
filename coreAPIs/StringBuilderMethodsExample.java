00122f1e7a15818723a8966945b8d957dfaed199
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-11-13 14:56:02 +0100

    Chapter 4 - Core APIs StringBuilder common methods

---
+++ coreAPIs/StringBuilderMethodsExample.java
@@ -0,0 +1,24 @@
+/**
+ * @author The Code Alchemist
+ * from Chapter 4: Core APIs, pages 172 - 175
+ * Important StringBuilder methods for the exam
+ */
+package coreAPIs;
+public class StringBuilderMethodsExample {
+    public static void main(String[] args) {
+
+        // common methods and indexes
+        var sb = new StringBuilder("animals");
+        int begin = sb.indexOf("a");
+        System.out.println(begin);      // 0, first letter of animals
+        int end = sb.indexOf("al");
+        System.out.println(end);        // 4, stops at al (not included)
+        String sub = sb.substring(sb.indexOf("a"), sb.indexOf("al"));   // start at a, stop at the al substring
+        System.out.println(sub);        // anim
+        int len = sb.length();
+        System.out.println(len);        // 7
+        char ch = sb.charAt(6);
+        System.out.println(ch);         // s
+        System.out.println(sub + " " + len + " " + ch); // anim 7 s
+    }
+}
