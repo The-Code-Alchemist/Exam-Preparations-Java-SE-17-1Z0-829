de370a9e1cdf4d638ab6a8e45e70500a9a45cdd9
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-11-15 18:54:48 +0100

    Chapter 4 - Core APIs examples added

--- coreAPIs/ArraysAndLoops.java
+++ coreAPIs/ArraysAndLoops.java
@@ -5,16 +5,37 @@
 package coreAPIs;
 public class ArraysAndLoops {
     public static void main(String[] args) {
+        showArrayNumbers();
+
+        // these two are equivalent
+        showMoreArrayNumbers();
+        showArrayNumbersEnhancedForLoop();
+    }
+
+    private static void showArrayNumbers() {
+        var numbers = new int[10];
+        for(int i = 0; i < numbers.length; i++) {           // <= instead of < leads to an Exception, be careful on the exam
+            // prints the index of this array increased by 5
+            System.out.print((numbers[i] = i + 5) + " ");   // 5 6 7 8 9 10 11 12 13 14
+        }
+        System.out.println("");
+    }
+
+    private static void showMoreArrayNumbers() {
         var numbers = new int[10];
         for(int i = 0; i < numbers.length; i++) {
-            // prints the index of this array increased by 5
-            System.out.print(numbers[i] = i + 5);
-        }   System.out.println("");
-
-        var moreNumbers = new int[10];
-        for(int j = 0; j < moreNumbers.length; j++) {
             // this one only prints 5
-            System.out.print(moreNumbers[j] += 5);
+            System.out.print((numbers[i] += 5) + " ");      // 5 5 5 5 5 5 5 5 5 5
         }
+        System.out.println("");
+    }
+
+    private static void showArrayNumbersEnhancedForLoop() {
+        var numbers = new int[10];
+        for(int i : numbers) {
+            // this one only prints 5
+            System.out.print((numbers[i] + 5) + " ");   // 5 5 5 5 5 5 5 5 5 5
+        }
+        System.out.println("");
     }
 }
