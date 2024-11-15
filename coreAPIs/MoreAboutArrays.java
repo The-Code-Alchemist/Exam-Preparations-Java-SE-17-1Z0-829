8222d6114ab07d4f8075708b0ca9a24b5eac0d2d
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-11-15 15:02:29 +0100

    Chapter 4 - Core APIs String and StringBuilder equality examples with explanations. Rearranged code into methods.

--- coreAPIs/MoreAboutArrays.java
+++ coreAPIs/MoreAboutArrays.java
@@ -1,30 +1,20 @@
 /**
  * @author The Code Alchemist
- * Chapters 4: Core APIs, page 179, 180, 181, 182
+ * Chapters 4: Core APIs, page 178 - 182
+ * All elements of an array are set to its default value such as int with a default value of 0
  */
 package coreAPIs;
+
+import java.util.Arrays;
+
 public class MoreAboutArrays {
     public static void main(String[] args) {
-        // the default value for the elements in this int array are set to 0
-        int[] numbers = new int[3];
-        int[] moreNumbers = new int[] {42, 55, 99};
-        int[] evenMoreNumbers = {42, 55, 99};       // does exactly the same as the line above
-        for (int i = 0; i <= 2; i++) {
-            System.out.print(numbers[i] + " ");
-            System.out.println(moreNumbers[i]);
-            System.out.println(evenMoreNumbers[i]);
-        }
+        // You can comment out these methods to see what each of them does
+        executeNumbersArray();
+        showArrayLengthAndMembers();
+        showAliasAndReference();
 
-        String[] bugs = {"cricket", "", ""};
-        String[] alias = bugs;
-        for (int i = 0; i <= 2; i++) {
-            System.out.print(bugs.equals(alias) + " ");
-            System.out.print((bugs == alias) + " ");
-            System.out.print(bugs.toString() + " ");
-            System.out.println(alias.toString());
-        }
-
-        // Defining multiple String arrays of multiple dimensions are defined here
+        // Multiple String arrays of multiple dimensions can be defined in a single line as demonstrated
         String one[], two[][], three[][][];
 
         // Arrays
@@ -34,7 +24,22 @@ public class MoreAboutArrays {
 //      againStrings[0] = new StringBuilder();          // Will not compile as it is
         againStrings[0] = String.valueOf(new StringBuilder());
         againStrings[0] = new String();
+    }
 
+    private static void showAliasAndReference() {
+        String[] bugs = {"cricket", "beetle", "ladybug"};
+        String[] alias = bugs;
+        for (int i = 0; i <= 2; i++) {
+            System.out.print(bugs.equals(alias) + " "); // true
+            System.out.print((bugs == alias) + " ");    // true
+            System.out.print(bugs.toString() + " ");    // array reference shown starting wit [L
+            System.out.println(alias.toString());       // array reference shown starting wit [L
+        }
+        System.out.println(Arrays.toString(bugs));      // Print an array clearly: [cricket, beetle, ladybug]
+        System.out.println(Arrays.toString(alias));     // Print an array clearly: [cricket, beetle, ladybug]
+    }
+
+    private static void showArrayLengthAndMembers() {
         String[] mammals = {"monkey", "chimp", "donkey"};
         System.out.println(mammals.length);
         System.out.println(mammals[0]);
@@ -44,5 +49,17 @@ public class MoreAboutArrays {
         var birds = new String[6];
         System.out.println(birds);          // reference to the String array birds
         System.out.println(birds.length);   // 6
+    }
+
+    private static void executeNumbersArray() {
+        // the default value for the elements in this int array are set to 0
+        int[] numbers = new int[3];
+        int[] moreNumbers = new int[] {42, 55, 99};
+        int[] evenMoreNumbers = {42, 55, 99};   // does exactly the same as the line above, essentially its shortcut
+        for (int i = 0; i <= 2; i++) {
+            System.out.print(numbers[i] + " ");
+            System.out.println(moreNumbers[i]);
+            System.out.println(evenMoreNumbers[i]);
+        }
     }
 }
