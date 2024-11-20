0a00d96f1c9cc91136ba4943a29cdc17ab3bf08a
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-11-20 14:05:19 +0100

    Chapter 4 - Core APIs String variables updated for explanation purposes

--- coreAPIs/DaylightSavingTime.java
+++ coreAPIs/DaylightSavingTime.java
@@ -14,38 +14,38 @@ import java.time.ZonedDateTime;
 public class DaylightSavingTime {
     public static void main(String[] args) throws Exception {
         var date = LocalDate.of(2022, Month.MARCH, 13);
-        System.out.println(date);
+        System.out.println("date:" + date);
 
         var time = LocalTime.of(1, 30);
-        System.out.println(time);
+        System.out.println("time: " + time);
 
         var zone = ZoneId.of("Europe/Amsterdam");
         System.out.println(zone);
 
         var dateTime = ZonedDateTime.of(date, time, zone);
-        System.out.println(dateTime);
-        System.out.println(dateTime.getHour());
-        System.out.println(dateTime.getOffset());
+        System.out.println("dateTime: " + dateTime);
+        System.out.println("dateTime hour: " + dateTime.getHour());
+        System.out.println("dateTime offset: " + dateTime.getOffset());
 
         dateTime = dateTime.plusHours(1);
-        System.out.println(dateTime);
-        System.out.println(dateTime.getHour());
-        System.out.println(dateTime.getOffset());
+        System.out.println("dateTime:" + dateTime);
+        System.out.println("dateTime hour: " + dateTime.getHour());
+        System.out.println("dateTime offset: " + dateTime.getOffset());
 
         date = LocalDate.of(2022, Month.NOVEMBER, 6);
         time = LocalTime.of(1, 30);
         zone = ZoneId.of("Europe/Amsterdam");
         dateTime = ZonedDateTime.of(date, time, zone);
-        System.out.println(dateTime);
+        System.out.println("dateTime: " + dateTime);
         dateTime = dateTime.plusHours(1);
-        System.out.println(dateTime);
+        System.out.println("dateTime: " + dateTime);
         dateTime = dateTime.plusHours(1);
-        System.out.println(dateTime);
+        System.out.println("dateTime + 1 hour: " + dateTime);
 
         date = LocalDate.of(2022, Month.MARCH, 13);
         time = LocalTime.of(2, 30);
         zone = ZoneId.of("Europe/Amsterdam");
         dateTime = ZonedDateTime.of(date, time, zone);
-        System.out.println(dateTime);
+        System.out.println("ZonedDateTime: " + dateTime);
     }
 }
