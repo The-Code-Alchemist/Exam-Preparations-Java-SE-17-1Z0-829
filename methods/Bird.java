968a9685a87835dc104c4ad3a396036b49f9d723
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-11-25 16:22:34 +0100

    Chapter 5 - Methods comment updated

--- methods/Bird.java
+++ methods/Bird.java
@@ -1,6 +1,6 @@
 /**
  * @author The Code Alchemist
- * Chapters 5: Methods, page 228
+ * Chapter 5: Methods, page 228
  */
 package methods;
 public class Bird {
@@ -8,7 +8,7 @@ public class Bird {
 
     /**
      * This method will not compile without body which is without curly braces {}
-     * The next line would work in an abstract class, though
+     * The next line would work in an abstract class as an abstract method, though
      * public abstract void fly2();
      */
 //    public void fly2()
