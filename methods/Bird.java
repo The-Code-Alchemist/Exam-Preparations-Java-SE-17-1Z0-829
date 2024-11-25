f49ed435bb47658968b4741bfe84a8e5b61cef63
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-11-25 15:52:30 +0100

    Chapter 5 - Methods Method body examples

---
+++ methods/Bird.java
@@ -0,0 +1,10 @@
+/**
+ * @author The Code Alchemist
+ * Chapters 5: Methods, page 228
+ */
+package methods;
+public class Bird {
+    public void fly1() {}       // Valid method declaration without parameters
+//    public void fly2()        // This method will not compile without body which is without curly braces {}
+    public void fly3(int a) {}  // Valid method declaration with one int parameter
+}
