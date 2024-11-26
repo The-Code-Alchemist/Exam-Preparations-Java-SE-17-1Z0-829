f4bc06abdd7d98b21ea0df89fc8e5006913b2ec9
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-11-26 15:38:19 +0100

    Chapter 5 - Methods Varargs parameter array lengths See what happens when null is passed explicitly

--- methods/DogWalker.java
+++ methods/DogWalker.java
@@ -1,18 +1,26 @@
 /**
  * @author The Code Alchemist
  * from Chapter 5: Methods, page 234
- * vararg example
+ * varargs example
  */
 package methods;
 public class DogWalker {
     private static void walkDog(int start, int... steps) {
-
+        System.out.print("First parameter " + start + " ");
+        System.out.println("Varargs array length " + steps.length);
     }
 
     public static void main(String[] args) {
-        walkDog(1);                     // array of length 0
-        walkDog(1, 2);           // array of length 1
-        walkDog(1, 2, 3);        // array of length 2
-        walkDog(1, new int[] {4 ,5});   // array of length 2
+        walkDog(1);                    // array of length 0
+        walkDog(1, 2);          // array of length 1
+        walkDog(1, 2, 3);       // array of length 2
+        walkDog(1, new int[] {4 ,5});  // array of length 2
+
+        /**
+         * null can be passed explicitly as a varargs parameter e.g. of type int
+         * Although the code compiles, the program throws a NullPointerException at runtime
+         * This is because the length of null can't be determined
+         */
+        walkDog(1, null);
     }
 }
