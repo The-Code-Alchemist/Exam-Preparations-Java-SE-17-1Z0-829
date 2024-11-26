8898e0fa09146efd4d44fcdfc655e2d34aae0711
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-11-26 14:48:02 +0100

    Chapter 5 - Methods Varargs parameters explained

--- methods/VisitAttractions.java
+++ methods/VisitAttractions.java
@@ -2,11 +2,35 @@
  * @author The Code Alchemist
  * from Chapter 5: Methods, page 233
  * Some methods with vararg parameters
- * A method can at most have one vararg parameter passed to it, the last one. Otherwise the method will not compile.
+ * A method can at most have one varargs parameter passed to it, the last one. Otherwise, the method will not compile.
  */
 package methods;
 public class VisitAttractions {
-    public void walk1(int... steps) {}
+
+    public static void main(String[] args) {
+
+        /**
+         * When calling a method with a varargs parameter, you can pass in an array
+         */
+        int[] data = new int[] {1, 2, 3};
+        walk1(data);
+
+        /**
+         * When calling a method with a varargs parameter, you can list the elements of the array and let Java create it for you
+         */
+        walk1(1, 2, 3);
+
+        /**
+         * When you omit the varargs values in the method call altogether, Java will create an empty array of length zero
+         */
+        walk1();
+    }
+
+    // walk1 made static in order to call it from the main method
+    public static void walk1(int... steps) {
+        int[] step2 = steps;    // not necessary, but shows steps is of type int[]
+        System.out.println(step2.length);
+    }
     public void walk2(int start, int... steps) {}
 //    public void walk3(int... steps, int start) {}     // will not compile, only the last parameter can be a vararg
 //    public void walk4(int... steps, int... start) {}  // will not compile, only one vararg parameter allowed - the last
