7e80bd5feab13bd1439da9bac44054e9c4c6f6ae
The Code Alchemist <onur.akgun@code-alchemists.com>
2024-11-28 19:33:40 +0100

    Chapter 5 - Methods Accessing public fields - allowed from any packing, no inheritance required

--- pond/duck/DuckTeacher.java
+++ pond/duck/DuckTeacher.java
@@ -1,8 +1,16 @@
+/**
+ * @author The Code Alchemist
+ * from Chapter 5: Methods, page 242
+ * This class has only public members, it allows access to any class that wants it.
+ */
 package pond.duck;
-
 public class DuckTeacher {
     public String name = "helpful";
     public void swim() {
         System.out.println(name);   // public access is OK
+    }
+
+    public static void main(String[] args) {
+        new DuckTeacher().swim();
     }
 }
--- pond/goose/LostDuckling.java
+++ pond/goose/LostDuckling.java
@@ -1,9 +1,17 @@
+/**
+ * @author The Code Alchemist
+ * from Chapter 5: Methods, pages 242
+ */
 package pond.goose;
 import pond.duck.DuckTeacher;
 public class LostDuckling {
     public void swim() {
         var teacher = new DuckTeacher();
-        teacher.swim();
-        System.out.println("Thanks " + teacher.name);
+        teacher.swim();                                 // allowed
+        System.out.println("Thanks " + teacher.name);   // allowed
+    }
+
+    public static void main(String[] args) {
+        new LostDuckling().swim();
     }
 }
