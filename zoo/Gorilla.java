862ccfd3a4b8da5d9760b144f5e01e9b4abcdf6d
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-01-09 17:21:10 +0100

    Chapter 5 - Methods instance and static methods, demonstrating which can call which

--- zoo/Gorilla.java
+++ zoo/Gorilla.java
@@ -1,8 +1,8 @@
-package zoo;
 /**
  * @author The Code Alchemist
  * Chapters 5 and 6: Methods and Class Design, page 247, 293, 294
  */
+package zoo;
 public class Gorilla extends Animal {
     public Gorilla(int age) {
         super(age, "Gorilla");     // calls the first Animal constructor
