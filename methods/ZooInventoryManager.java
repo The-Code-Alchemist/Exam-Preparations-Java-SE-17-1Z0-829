156e82dea76e6566c1a2592a9392b139b7191984
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-01-10 10:45:34 +0100

    Chapter 5 - Methods static final examples ClassCastException example Comment and value updated

--- methods/ZooInventoryManager.java
+++ methods/ZooInventoryManager.java
@@ -11,7 +11,9 @@ public class ZooInventoryManager {
     public static void main(String[] args) {
         treats[0] = "popcorn";
         System.out.println(treats[0]);
-        treats[0] = "";     // contents can be reassigned, just don't point to a different object
+        treats[0] = "!";                    // Contents can be reassigned, just don't point to a different object
+        treats[1] = (String) new Object();  // Reference to a different kind of object will cause a ClassCastException
         System.out.println(treats[0]);
+        System.out.println(treats[1]);
     }
 }
--- methods/ZooPen.java
+++ methods/ZooPen.java
@@ -2,13 +2,14 @@
  * @author The Code Alchemist
  * Chapter 5: Methods, page 249
  * static final CONSTANT example
+ * final variables cannot ber reassigned, regardless of instance or static types
  */
 package methods;
 public class ZooPen {
     private static final int NUM_BUCKETS = 45;
 
     public static void main(String[] args) {
-//        NUM_BUCKETS = 5;                  // final variable cannot be rreassigned
+//        NUM_BUCKETS = 5;                  // final variable cannot be reassigned
         System.out.println(NUM_BUCKETS);    // they can be used though
     }
 }
