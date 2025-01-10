5e9b54adc806d7ccb56c96caa4c6b335233e277a
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-01-10 13:10:15 +0100

    Chapter 5 - Methods static initializers illustrated main method moved to the bottom

--- methods/StaticInitializerExample.java
+++ methods/StaticInitializerExample.java
@@ -1,7 +1,9 @@
 /**
  * @author The Code Alchemist
  * Chapter 5: Methods, page 249
- * static initializer example
+ * static initializers are run when the class is first loaded.
+ * If there are multiple static initializers in the class, they are executed in the order of their appearance.
+ * From top to bottom.
  */
 package methods;
 public class StaticInitializerExample {
@@ -17,21 +19,24 @@ public class StaticInitializerExample {
                 = NUM_SECONDS_PER_MINUTE * NUM_MINUTES_PER_HOUR;
     }
 
-    public static void main(String[] args) {
-        System.out.println(NUM_SECONDS_PER_MINUTE);
-        System.out.println(NUM_MINUTES_PER_HOUR);
-        System.out.println(NUM_SECONDS_PER_HOUR);
-    }
-
     private static int one;
-    private static final int two;
-    private static final int three = 3;
-//  private static final int four;  // will not compile, it's never been initialized
 
+    private static final int two;   // Not initialized, but will be in the static initializer on line  29
+    private static final int three = 3;
+//  private static final int four;  // will not compile, four has not been initialized
     static {
         one = 1;
         two = 2;
 //        three = 3;  // final cannot be assigned a new value, not even the same value
 //        two = 4 ;   // final value cannot be assigned a new value
+    }
+
+    public static void main(String[] args) {
+        System.out.println(NUM_SECONDS_PER_MINUTE);
+        System.out.println(NUM_MINUTES_PER_HOUR);
+        System.out.println(NUM_SECONDS_PER_HOUR);
+
+        System.out.println(one);
+        System.out.println(two);
     }
 }
