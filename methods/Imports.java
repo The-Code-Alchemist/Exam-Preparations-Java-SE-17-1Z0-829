c38358cb37c4a7829097d4e6f92199bf23efe228
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-01-13 10:40:40 +0100

    Chapter 5 - Methods Comment updated, Math.PI example added

--- methods/Imports.java
+++ methods/Imports.java
@@ -1,15 +1,18 @@
 /**
  * @author The Code Alchemist
  * Chapter 5: Methods, page 251
- * static import example
+ * static imports are for importing static members such as variables and methods which comes in handy when referring to
+ * a lot of constants in another class e.g. java.lang.Math.PI
  */
 package methods;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Arrays;
 
 public class Imports {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("one","two");
+        System.out.println(list);
+        System.out.println(Math.PI);
     }
 }
