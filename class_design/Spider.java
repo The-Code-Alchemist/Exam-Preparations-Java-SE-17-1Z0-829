6499ba1d7d71f06d8516c739e0358795710c1841
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-01-16 14:20:27 +0100

    Chapter 6 - Class Design protected fields are not accessible

---
+++ class_design/Spider.java
@@ -0,0 +1,17 @@
+/**
+ * @author The Code Alchemist
+ * Chapter 6: Class Design, page 277
+ * Inheritance gone wrong example
+ *
+ * Field size is not accessible here, because class_design.BigCat is not inherited
+ *
+ * Every class implicitly extends java.lang.Object
+ * This class explicitly extends java.lang.Object
+ */
+package class_design;
+
+public class Spider extends Object {
+    public void printDetails() {
+//        System.out.println(size);     // not accessible here, Spider does not extend BigCat
+    }
+}
