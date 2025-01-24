567722d88a6ade503577f29b04299a48ef8de8d3
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-01-24 12:14:54 +0100

    Chapter 6 - Class Design this reference example comment update

--- class_design/zoo/Flamingo.java
+++ class_design/zoo/Flamingo.java
@@ -7,7 +7,8 @@ package class_design.zoo;
 public class Flamingo {
     private String color = null;
     public void setColor(String color) {
-        color = color;          // makes line 14 print PINK when replaced with | this.color = color |
+        // assigns the already assigned value null to itself
+        color = color;          // makes line 15 print PINK when replaced with | this.color = color |
     }
     public static void main(String[] args) {
         var f = new Flamingo();
