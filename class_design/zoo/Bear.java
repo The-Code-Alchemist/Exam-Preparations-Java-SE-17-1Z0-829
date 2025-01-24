3038ad17c97efb66b10759eaf4e11db64be055c1
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-01-24 11:49:31 +0100

    Chapter 6 - Class Design Top-level class with multiple classes in a single source file

---
+++ class_design/zoo/Bear.java
@@ -0,0 +1,15 @@
+/**
+ * @author The Code Alchemist
+ * Chapter 6: Class Design, pages 282
+ * A .java source can contain as many classes, but can have at most one public class
+ * Bear is the only class that can be public In this example, since it matches the name of the sourc file Bear.java
+ *
+ * Inner classes can be private such as BearCub on line 12
+ */
+package class_design.zoo;
+public class Bear {
+    // inner classes can  will be discussed later in the next chapter: Beyond Classes
+    private class BearCub {}
+}
+class Bird {}
+class Fish {}
