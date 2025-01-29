8bf34e5b53fff95c0a7d158d33b3fa85b2a9ae45
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-01-29 11:40:08 +0100

    Chapter 6 - Class Design this reference example comment update

--- class_design/zoo/Flamingo.java
+++ class_design/zoo/Flamingo.java
@@ -1,14 +1,17 @@
 /**
  * @author The Code Alchemist
- * Chapter 6: Class Design, pages 283
+ * Chapter 6: Class Design, page 283
  * this reference example
+ *
+ * In order to set the instance variable with the method variable, use the following line
+ * this.color = color;
  */
 package class_design.zoo;
 public class Flamingo {
     private String color = null;
     public void setColor(String color) {
         // assigns the method parameter to itself
-        color = color;          // makes line 15 print PINK when replaced with | this.color = color |
+        color = color;          // makes line 15 print PINK when replaced with | this.color = color;
     }
     public static void main(String[] args) {
         var f = new Flamingo();
