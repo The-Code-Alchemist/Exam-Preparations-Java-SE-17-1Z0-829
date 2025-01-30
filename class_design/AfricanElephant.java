cd6dba0eaab378f3fbcf7000f6306576aa1c009b
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-01-31 00:32:56 +0100

    Chapter 6 - Class Design Call to parent constructors super(), classes moved to chapter-specific packages, comments updated

--- class_design/AfricanElephant.java
+++ class_design/AfricanElephant.java
@@ -3,5 +3,5 @@
  * Chapter 6: Class Design, page 296
  * constructors - includes a no-args constructor even if its parent Elephant has one
  */
-package zoo;
-public class AfricanElephant extends Elephant{}
+package class_design;
+public class AfricanElephant extends Elephant {}
