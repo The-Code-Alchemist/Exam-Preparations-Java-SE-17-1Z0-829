5b320323a9b4eb47b37e51cd6cddedb002bdc855
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-01-30 21:30:40 +0100

    Chapter 6 - Class Design moved to chapter-specific package, comment updated

--- class_design/zoo/Donkey.java
+++ class_design/zoo/Donkey.java
@@ -1,9 +1,9 @@
 /**
  * @author The Code Alchemist
  * Chapter 6: Class Design, page 294, 295
- * Constructor equivalency - the declared class is equivalent to the commented out code
+ * Constructor equivalency - the declared class is equivalent to the commented out code fragments
  */
-package zoo;
+package class_design.zoo;
 
 // public class Donkey {}
 
