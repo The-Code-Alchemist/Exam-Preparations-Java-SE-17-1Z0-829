6fe6e97a8ff598c22af7aae97a8d514955dae475
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-02-04 23:45:06 +0100

    Chapter 6 - Class Design moved to reptiles package

--- class_design/zoo/reptiles/Reptile.java
+++ class_design/zoo/reptiles/Reptile.java
@@ -3,7 +3,7 @@
  * Chapter 6: Class Design, page 284
  * super reference - look at the Crocodile class
  */
-package class_design.zoo.Reptiles;
+package class_design.zoo.reptiles;
 public class Reptile {
     protected int speed = 10;
 }
