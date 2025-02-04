b2af82777c27d9f96894ae8274610dbf32767e52
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-02-04 22:20:44 +0100

    Chapter 6 - Class Design Overriding the method of a parent class page number update

--- class_design/zoo/camels/Camel.java
+++ class_design/zoo/camels/Camel.java
@@ -1,6 +1,6 @@
 /**
  * @author The Code Alchemist
- * Chapter 6: Class Design, pages 306 - 308
+ * Chapter 6: Class Design, pages 306 - 307
  * Overriding example with access modifiers
  */
 package class_design.zoo.camels;
--- class_design/zoo/camels/Rider.java
+++ class_design/zoo/camels/Rider.java
@@ -1,6 +1,6 @@
 /**
  * @author The Code Alchemist
- * Chapter 6: Class Design, pages 306 - 308
+ * Chapter 6: Class Design, pages 306 - 307
  * Overriding example with access modifiers
  *
  * Rider is included in this package to illustrate overriding
