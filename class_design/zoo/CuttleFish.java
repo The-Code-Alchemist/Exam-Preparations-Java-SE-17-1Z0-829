4fe8f96a12fb0174fb7cf6e1db1329f6b486ca8c
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-02-04 09:47:19 +0100

    Chapter 6 - Class Design Order of initialization illustrated
    Class moved to chapter-specific package
    
    Comment updated

---
+++ class_design/zoo/CuttleFish.java
@@ -0,0 +1,29 @@
+/**
+ * @author The Code Alchemist
+ * Chapter 6: Class Design, pages 302 - 303
+ * constructor calls and order of initialization
+ *
+ * 1. Static variables and initializers are processed first     0
+ * 2. main method comes after that                              Ready
+ * 3. Instance variables and initializers are processed         Swimmy
+ *                                                              1
+ * 4. Constructor creates a CuttleFish object, printing         Constructor
+ */
+package class_design.zoo;
+public class CuttleFish {
+    private static String name = "swimmy";
+    { System.out.println(name); }
+    private static int COUNT = 0;
+    static { System.out.println(COUNT); }
+    { COUNT++; System.out.println(COUNT); }
+
+    public CuttleFish() {
+        System.out.println("Constructor");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Ready");
+        new CuttleFish();
+    }
+
+}
