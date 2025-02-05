cb4c6ffc0a21890ef14310ed14695f0f3fcebc77
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-02-05 10:46:41 +0100

    Chapter 6 - Class Design Overriding with a covariant return type in a subclass

---
+++ class_design/rhinos/JavanRhino.java
@@ -0,0 +1,18 @@
+/**
+ * @author The Code Alchemist
+ * Chapter 6: Class Design, page 309
+ * Overriding example with covariant return type
+ */
+package class_design.rhinos;
+public class JavanRhino extends Rhino {
+    // String is a subtype of CharSequence
+    // This makes it covariant with the return type CharSequence of the class Rhino
+    public String getName() {
+        return "java rhino";
+    }
+
+    // The overridden getColor() method will not compile with the CharSequence return type
+    public String getColor() {
+        return "grey";
+    }
+}
---
+++ class_design/rhinos/Rhino.java
@@ -0,0 +1,14 @@
+/**
+ * @author The Code Alchemist
+ * Chapter 6: Class Design, page 309
+ * Overriding example with covariant return type
+ */
+package class_design.rhinos;
+public class Rhino {
+    protected CharSequence getName() {
+        return "rhino";
+    }
+    protected String getColor() {
+        return "grey, black, or white";
+    }
+}
