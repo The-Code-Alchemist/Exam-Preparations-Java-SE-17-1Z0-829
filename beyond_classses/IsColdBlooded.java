1468fb96d6fb818f717b46f83671d01cf3e9295c
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-02-21 15:23:46 +0100

    Chapter 7 - Beyond Classes Overriding default methods is optional

---
+++ beyond_classses/IsColdBlooded.java
@@ -0,0 +1,12 @@
+/**
+ * @author The Code Alchemist
+ * Chapter 7: Beyond Classes, page 354 - 355
+ * default method example
+ */
+package beyond_classses;
+public interface IsColdBlooded {
+    boolean hasScales();
+    default double getTemperatures() {
+        return 10.0;
+    }
+}
---
+++ beyond_classses/Snake.java
@@ -0,0 +1,19 @@
+/**
+ * @author The Code Alchemist
+ * Chapter 7: Beyond Classes, page 354 - 355
+ * default method example
+ * Overriding the getTemperatures() method is optional
+ */
+package beyond_classses;
+public class Snake implements IsColdBlooded{
+    @Override
+    public boolean hasScales() {
+        return true;
+    }
+
+    // Overriding this method is optional, because it already has a default implementation
+    @Override
+    public double getTemperatures() {
+        return 12;
+    }
+}
