35e2d6511d2d5ea4bd0ddf7832fbfb5d139b21af
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-02-25 09:40:36 +0100

    Chapter 7 - Beyond Classes Subclasses HAVE to extend their permitting sealed superclass

---
+++ beyond_classses/sealed_classes/penguins/Emperor.java
@@ -0,0 +1,9 @@
+/**
+ * @author The Code Alchemist
+ * Chapter 7: Beyond Classes, pages 368
+ * A permitted subclass has to extend a sealed class
+ */
+package beyond_classses.sealed_classes.penguins;
+public final class Emperor extends Penguin {
+    // This class will not compile if it does not extend its parent class Penguin
+}
---
+++ beyond_classses/sealed_classes/penguins/Penguin.java
@@ -0,0 +1,8 @@
+/**
+ * @author The Code Alchemist
+ * Chapter 7: Beyond Classes, pages 368
+ * A sealed class need to be declared and compiled in the same package as its direct subclasses
+ */
+package beyond_classses.sealed_classes.penguins;
+public sealed class Penguin permits Emperor {
+}
