64d865f49ba662c19a7386a0428607a755f4ef6a
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-02-25 10:46:40 +0100

    Chapter 7 - Beyond Classes comment update

--- beyond_classses/sealed_classes/final_subclass/George.java
+++ beyond_classses/sealed_classes/final_subclass/George.java
@@ -1,7 +1,7 @@
 /**
  * @author The Code Alchemist
  * Chapter 7: Beyond Classes, pages 369
- * A f
+ * A final class can extend a parent class, but cannot itself be extended any further itself
  */
 package beyond_classses.sealed_classes.final_subclass;
 public class George
