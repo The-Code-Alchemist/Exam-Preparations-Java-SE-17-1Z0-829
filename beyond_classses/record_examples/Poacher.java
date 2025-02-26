af46f526733e3897b1ea3e4d64dd8da98b80b7e7
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-02-26 09:57:02 +0100

    Chapter 7 - Beyond Classes Encapsulation and immutability example

---
+++ beyond_classses/record_examples/Poacher.java
@@ -0,0 +1,16 @@
+/**
+ * @author The Code Alchemist
+ * Chapter 7: Beyond Classes, pages 374 - 375
+ * The Poacher class cannot change the numberEggs value, such as on line 10
+ * Since a setter is not present in the Crane class, which means the numberEggs and name variables
+ * can only be set when creating a new Crane object
+ */
+package beyond_classses.record_examples;
+public class Poacher {
+    public void badActor() {
+        var mother = new Crane(5, "Cathy");
+     // mother.numberEggs = -1;
+        System.out.println(mother.getNumberEggs());
+
+    }
+}
