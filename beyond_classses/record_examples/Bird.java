8e28db9295758640c2f04f68bee6092f121affd3
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-02-26 22:14:52 +0100

    Chapter 7 - Beyond Classes A record can implement a regular or sealed interfaces, provided that their abstract methods are implemented
    Long constructor added

---
+++ beyond_classses/record_examples/Bird.java
@@ -0,0 +1,6 @@
+/**
+ * @author The Code Alchemist
+ * Chapter 7: Beyond Classes, page 378
+ */
+package beyond_classses.record_examples;
+public interface Bird {}
--- beyond_classses/record_examples/Crane.java
+++ beyond_classses/record_examples/Crane.java
@@ -1,11 +1,22 @@
 /**
  * @author The Code Alchemist
- * Chapter 7: Beyond Classes, pages 376
+ * Chapter 7: Beyond Classes, pages 376, 378
  * A record is a data-only or data carrier class which reduce boilerplate code.
- * Records are always public and final
+ * Records are implicitly public and final
  * Records separate data from operations and cannot extend classes
  *
  * Have a look at this record, it creates the data prviously handled by the CranePOJO class in a single line (11)
+ *
+ * Records can implement regular or sealed interfaces, but they need to implement its abstract methods
  */
 package beyond_classses.record_examples;
-public record Crane(int numberEggs, String name) {}
+public record Crane(int numberEggs, String name) implements Bird {
+
+    // The long constructor automatically inserted by the Java compiler
+    public Crane(int numberEggs, String name) {
+        // Since each field is fina
+        if (numberEggs < 0 ) throw new IllegalArgumentException();
+        this.numberEggs = numberEggs;
+        this.name = name;
+    }
+}
