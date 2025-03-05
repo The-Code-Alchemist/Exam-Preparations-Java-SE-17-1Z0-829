88e1e8a0025d7f54713838b7e6e12ef64e09102f
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-03-05 12:53:12 +0100

    Chapter 7 - Beyond Classes Casting example with incompatible types and the instanceof operator page update

--- beyond_classses/understanding_polymorphism/Bird.java
+++ beyond_classses/understanding_polymorphism/Bird.java
@@ -1,6 +1,6 @@
 /**
  * @author The Code Alchemist
- * Chapter 7: Beyond Classes, pages 395 - 396
+ * Chapter 7: Beyond Classes, pages 395 - 397
  */
 package beyond_classses.understanding_polymorphism;
 public class Bird { }
