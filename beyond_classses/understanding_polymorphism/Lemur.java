0fc993c042e8d61d7e04b45f04317c7f243ad292
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-03-05 10:13:16 +0100

    Chapter 7 - Beyond Classes Casting example added

--- beyond_classses/understanding_polymorphism/Lemur.java
+++ beyond_classses/understanding_polymorphism/Lemur.java
@@ -1,13 +1,15 @@
 /**
  * @author The Code Alchemist
- * Chapter 7: Beyond Classes, pages 392 - 393
+ * Chapter 7: Beyond Classes, pages 392 - 395
  *
  * The property of an object can take many different forms.
  * A cast is not required if the object is being reassigned to a supertype or interface.
  * Only one object, Lemur is created. The same object exists in memory, regardless of which reference is pointing to it.
  *
  * Polymorphism enables an instance of Lemur to be reassigned or passed to a method using of it supertypes,
- * such asPrimate or HasTail
+ * such asPrimate or HasTail.
+ *
+ * Casting a reference from a supertype to a subtype requires an explicit cast, line 43
  */
 package beyond_classses.understanding_polymorphism;
 public class Lemur extends Primate implements HasTail {
@@ -35,5 +37,10 @@ public class Lemur extends Primate implements HasTail {
           Without an explicit case back to Lemur, we no longer have access to the Lemur properties ogf the object
          */
         Object lemurAsObject = lemur;
+
+        // Casting Objects
+        Primate anotherPrimate = new Lemur();           // Implicit cast to supertype
+        Lemur anotherLemur = (Lemur) anotherPrimate;    // Explicit cast to subtype gains access to all of the methods and fields in the Lemur class
+     // Lemur anotherLemurAgain = anotherPrimate;       // Will not compile without an explicit cast
     }
 }
