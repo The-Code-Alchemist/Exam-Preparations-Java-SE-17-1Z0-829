9d86545da0df77f9a085c31819ed78c411f45ea3
The Code Alchemist <onur.akgun@code-alchemists.com>
2025-03-06 23:20:53 +0100

    Chapter 8 - Lambdas and Functional Interfaces
    Functional interface example moved to separate package

--- lambdas_and_functional_interfaces/functional/sprinting_tiger/Sprint.java
+++ lambdas_and_functional_interfaces/functional/sprinting_tiger/Sprint.java
@@ -4,7 +4,7 @@
  * A functional interface such as this one has exactly one abstract method
  * The annotation on line 8 tells the compiler that you intend for the code to be a functional interface.
  */
-package lambdas_and_functional_interfaces.functional;
+package lambdas_and_functional_interfaces.functional.sprinting_tiger;
 @FunctionalInterface
 public interface Sprint {
     public void sprint(int speed);
